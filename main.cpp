b3f155ad0025ac5afbf32277c018876c91c49b3c
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2021-01-30 14:31:43 +0900

    Add comments to explain how the precompiled header works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "my_file.h"
 
 int main() {
+  
+  std::cout << "Hello world\n";
+
   my_func();
+  my_func_inline();
+  
   return 0;
 }
--- my_common_lib_folder/my_file.h
+++ my_common_lib_folder/my_file.h
@@ -3,4 +3,8 @@
 
 void my_func();
 
+inline void my_func_inline() {
+  std::cout << "This also works!\n";
+}
+
 #endif // MY_FILE_H
